61c880656677f9490eeb63abda6c630acda81b88
Geoffrey Challen <challen@illinois.edu>
2020-05-17 18:53:17 -0500

    Fix index.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 export { LoginButton } from "./react-google-login/LoginButton"
 
+export { UpdateHash } from "./element-tracker/UpdateHash"
+export { SidebarMenu } from "./element-tracker/SidebarMenu"
+
 export { Lead } from "./mdx/Lead"
 export { Highlighted } from "./mdx/Highlighted"
 export { headings } from "./mdx/Headings"
